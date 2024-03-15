8a7aa0f862d146a3fbe51e57ec53081d74e058be
JShistory <behappy0916@naver.com>
2024-03-15 17:43:20 +0900

    feat: 게시글 수정 기능

--- src/main/java/com/study/freelecspringboot2webservice/web/IndexController.java
+++ src/main/java/com/study/freelecspringboot2webservice/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.study.freelecspringboot2webservice.web;
 
 import com.study.freelecspringboot2webservice.service.posts.PostsService;
+import com.study.freelecspringboot2webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -19,5 +21,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
